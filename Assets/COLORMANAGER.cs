3439562afc3e51a71469fbb4a2b20c09b3b96cb5
Martooooooo <166547016+Martooooooo@users.noreply.github.com>
2025-06-23 11:45:54 -0300

    hola

---
+++ Assets/COLORMANAGER.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COLORMANAGER : MonoBehaviour
+{
+    public Color[] colores;
+    public Renderer cubeRenderer;
+    public int randomColorIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(SetRandomColor),0,.5f);
+    }
+
+   void SetRandomColor()
+    {
+        randomColorIndex = Random.Range(0, colores.Length);
+        cubeRenderer.material.color = colores[randomColorIndex];
+    }
+
+}
